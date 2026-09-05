8da63a45f57c86ea7f7fd520c6e4d982db55b1b4
playThankun <iamthankun@gmail.com>
2026-09-05 15:33:00 +0900

    fdsafsdfasdf

--- webpack_config/prod.js
+++ webpack_config/prod.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
@@ -7,26 +9,41 @@ module.exports = {
     entry: {
         'entry.min': './src/entry.js',
     },
+
     mode: 'production',
+
     output: {
         chunkFilename: '[name].[contenthash].js',
         filename: '[name].js',
     },
+
     module: {
         rules: [],
     },
+
     plugins: [
         new MiniCssExtractPlugin({
             filename: '[name].css',
             chunkFilename: '[name][contenthash].css',
         }),
+
+        new HtmlWebpackPlugin({
+            template: path.resolve('example', 'example.ejs'),
+            title: 'Entry Example',
+            filename: path.resolve('dist', 'index.html'),
+            inject: false,
+            hash: true,
+        }),
     ],
+
     optimization: {
         minimize: true,
+
         minimizer: [
             new TerserPlugin({
                 include: /\.min\.js$/,
                 parallel: true,
+
                 terserOptions: {
                     ecma: 5,
                 },
