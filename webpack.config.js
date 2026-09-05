0226884c0a643296c136a76784717fe5bd55471b
playThankun <iamthankun@gmail.com>
2026-09-05 15:47:25 +0900

    dafsdfas

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,34 @@
 const { merge } = require('webpack-merge');
+
 const common = require('./webpack_config/common');
 
 let conf;
 
 switch (process.env.NODE_ENV) {
+
     case 'lint':
+
         // common config 을 가지지 않는다.
+
         return require('./webpack_config/lint');
+
     case 'production':
+
+        // common.js의 entry.js 빌드를 제거
+        delete common.entry;
+
         conf = require('./webpack_config/prod');
         break;
+
     case 'serve':
     case 'serve-mini':
+
         conf = require('./webpack_config/serve');
         break;
+
     case 'development':
     default:
+
         conf = require('./webpack_config/dev');
         break;
 }
